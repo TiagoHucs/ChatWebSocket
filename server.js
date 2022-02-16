3908e9938b99b0b4ea5e61d3eab9588e3da61c14
tiago.hucs <tiago.hucs@accenture.com>
2022-02-16 18:16:14 -0300

    inicial

---
+++ server.js
@@ -0,0 +1,32 @@
+const expresss = require('express');
+const path = require('path');
+
+const app = expresss();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(expresss.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'public'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+
+app.use('/',(req, res) => {
+    res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages',messages);
+
+    socket.on('sendMessage', data => {
+        console.log(data);
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage',data);
+    });
+});
+
+server.listen(3000);
+console.log(`Server listen on 3000`);
